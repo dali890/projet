3e11172bd8b25ec9b05561822b863e5d40d413d1
Amalkhemiri <amal.khemiri@esprit.tn>
2015-12-02 10:31:12 +0100

    aaaaa
    Signed-off-by: Amalkhemiri <amal.khemiri@esprit.tn>

--- src/main/java/com/objis/mavenapps/App.java
+++ src/main/java/com/objis/mavenapps/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Dali" );
-        System.out.println( "Hello World Dali2" );
+        System.out.println( "Hello World" );
+        System.out.println( "Hello World2" );
         
     }
 }
